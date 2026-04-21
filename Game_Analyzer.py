4f003dc29849645bd84c5592650537b9730ad8c8
Martin Blomström <mblomst@kth.se>
2026-04-21 16:18:14 +0200

    Feature: Added capture for the score

--- Game_Analyzer.py
+++ Game_Analyzer.py
@@ -1,12 +1,18 @@
 import mss
 import mss.tools
+import numpy as np
+import cv2
+import keyboard
+import pyautogui as pag
+
+spaceIsNotPressed = True
 
 def GetScreenResolution():
     with mss.mss() as sct:
         monitor = sct.monitors[1]
         return monitor["width"], monitor["height"]
 
-def TakeScreenShotAtSecionOfScreen(top_left, bottom_right):
+def TakeScreenShotAtSecionOfScreen(top_left, bottom_right, outputImageName):
     with mss.mss() as sct:
         region = {
             "left": top_left[0],
@@ -16,11 +22,19 @@ def TakeScreenShotAtSecionOfScreen(top_left, bottom_right):
         }
 
         img = sct.grab(region)
-        mss.tools.to_png(img.rgb, img.size, output="region.png")
+        mss.tools.to_png(img.rgb, img.size, output = outputImageName + ".png")
 
 screen_width, screen_height = GetScreenResolution()
 
 print(screen_width)
 print(screen_height)
 
-TakeScreenShotAtSecionOfScreen((0, 0), (screen_width, screen_height)) # in this case screenshot the section "entire screen"
+while spaceIsNotPressed:
+    if keyboard.is_pressed('space'):
+        spaceIsNotPressed = False
+        TakeScreenShotAtSecionOfScreen((0, 0), (screen_width, screen_height),"entireScreen")  # in this case screenshot the section "entire screen"
+
+        TakeScreenShotAtSecionOfScreen((1214, 313), (1277, 328), "Score")
+        # Top left: Point(x=1214, y=313)
+        # Bottom right: Point(x=1277, y=328)
+

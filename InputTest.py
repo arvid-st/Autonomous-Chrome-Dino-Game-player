d184c0a50792a35da5dd8e9105b979909c3cabd7
Arvid <arst@kth.se>
2026-04-30 09:03:21 +0200

    Fixes #7 made it so that InputTest can both start ducking and stop ducking with different functions, also pivoted to using only pynput as it is not necessary to also use pyautogui.

--- InputTest.py
+++ InputTest.py
@@ -1,11 +1,32 @@
 import time
-import pyautogui
 from pynput import keyboard
 
-#fungerar men man måste skapa en virtual environment med 'python3 -m venv venv' i project foldern, aktivera den med 'source venv/bin/activate' samt sedan ladda ner pyatogui och pynput med pip install 
+#fungerar men man måste skapa en virtual environment med 'python3 -m venv venv' i project foldern, aktivera den med 'source venv/bin/activate' samt sedan pynput med pip install 
 #Behövs troligen en bättre lösning då denna var lite krånglig.
 
+kb = keyboard.Controller()
+
 stop = False
+ducking = False
+
+def jump():
+    kb.press(keyboard.Key.space)
+    kb.release(keyboard.Key.space)
+    print("Jump")
+
+def start_duck():
+    global ducking
+    if not ducking:
+        kb.press(keyboard.Key.down)
+        ducking = True
+        print("Start duck")
+
+def stop_duck():
+    global ducking
+    if ducking:
+        kb.release(keyboard.Key.down)
+        ducking = False
+        print("Stop duck")
 
 def on_press(key):
     global stop
@@ -24,5 +45,8 @@ print("Starting in 5 seconds. Press 's' to stop.")
 time.sleep(5)
 
 while not stop:
-    pyautogui.press('space')
-    time.sleep(1)
+    start_duck()
+    time.sleep(0.5)
+    stop_duck()
+    jump()
+    time.sleep(0.5)
